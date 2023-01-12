a0337a69a3f1ed27ee33dbfbccd2be44488cc689
Ana Moreno <ana.moreno@codeup.com>
2023-01-12 14:40:32 -0600

    added switch statment to nested for loop that iterates through a 2D Array

--- Module 1 - Introduction to Java/6. Arrays/ForLoopOn2DArrays.java
+++ Module 1 - Introduction to Java/6. Arrays/ForLoopOn2DArrays.java
@@ -1,33 +1,33 @@
 public class ForLoopOn2DArrays {
     public static void main(String[] args) {
-        
 
         int[][] grades = {
-            {72,73,74,75},
-            {65,63,63,62},
-            {100,99,98,97}
+                { 72, 73, 74, 75 },
+                { 65, 63, 63, 62 },
+                { 100, 99, 98, 97 }
         };
 
-        for (int i = 0; i <grades.length; i++){
-            for(int j=0; j<grades[i].length; j++){
-                System.out.print( grades[i][j] + " ");
-
-                if (i==0){
-                    System.out.println("\n Harry's grades are: " + grades[0][j] + " ");
-                }
-                if (i==1){
-                    System.out.println("\n Ron's grades are: " + grades[1][j] + " ");
-                }
-                if (i==2){
-                    System.out.println("\n Her's grades are: " + grades[2][j] + " ");
-                }
+        for (int i = 0; i < grades.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.print("\n\tHarry: ");
+                    break;
+                case 1:
+                System.out.print("\tRon: ");
+                break;
+                case 2:
+                System.out.print("\tHermione: ");
+                break;
+                default: System.out.println("");
             }
-            
-           
-        };
+            for (int j = 0; j < grades[i].length; j++) {
+                System.out.print(grades[i][j] + " ");
 
+            }
+            System.out.println("\n");
 
-
+        }
+        ;
 
     }
 }
