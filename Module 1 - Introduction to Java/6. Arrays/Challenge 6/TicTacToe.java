ad3db9a3fe6020d4a7e346c866c3e3eaea9ef61d
Ana Moreno <ana.moreno@codeup.com>
2023-01-23 15:59:12 -0600

    content on board?

--- Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
+++ Module 1 - Introduction to Java/6. Arrays/Challenge 6/TicTacToe.java
@@ -19,6 +19,21 @@ public class TicTacToe {
 
     // Task 2: Call the function printBoard();
     printBoard(board);
+
+
+
+    for (int i = 0; i < 9; i++){
+     if(i % 2 == 0){
+      System.out.println("Turn: X");
+      int[] spot = askUser(board);
+      board[spot[0]][spot[1]] = 'X';
+     }else {
+      System.out.println("Turn: O");
+      int[] spot = askUser(board);
+      board[spot[0]][spot[1]]= 'O';
+     }
+     printBoard(board);
+    }
     /*
      * { Task 3: Loop through turns.
      * 
@@ -40,6 +55,11 @@ public class TicTacToe {
      * 
      * }
      */
+
+
+
+
+
 
     scan.close();
   }
@@ -84,7 +104,12 @@ public class TicTacToe {
    *         3. Return the row and column in an int[] array.
    * 
    */
-
+  public static int[] askUser(char[][] board){
+    System.out.print(" - pick a row and column number: ");
+    int row = scan.nextInt();
+    int element = scan.nextInt();
+    return new int[] {row, element};
+  }
   /**
    * Task 6 - Write a function that determines the winner
    * Function name - checkWin
