8661d53e49793538236f18a01335f87bc1dcbf0c
Ana Moreno <ana.moreno@codeup.com>
2023-01-25 13:58:19 -0600

    module 2

--- Module 2 - Object Oriented Programming/7. The World of Objects/Section7/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Section7/Main.java
@@ -1,16 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        //utilizing class of car
         Car nissan = new Car();
         nissan.make = "Nissan";
         nissan.price = 10000;
         nissan.year = 2020;
         nissan.color = "Green";
+        // same results usilizing a constructor
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 29999;
-        dodge.year = 2018;
-        dodge.color = "blue";
+        Car dodge = new Car("Dodge", 29999, 2018, "blue");
+    
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price +
                 ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
