a85f04aa99c5d20a1079f78c74078b87e2b3dac5
Ana Moreno <ana.moreno@codeup.com>
2023-01-30 12:13:57 -0600

    working on objects

--- Module 2 - Object Oriented Programming/7. The World of Objects/Folder/Car.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Folder/Car.java
@@ -1,6 +1,17 @@
 public class Car {
+    // object blueprint
     String make;
     double price;
     int year;
     String color;
+
+     // creating a constructor to create a car object and update its fields all in one line. A constructor runs as soon as I create an object. The purpose is to update an objects fields
+
+     public Car(String make, double price, int year, String color){
+        //matching objects to the fields
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+     }
 }
--- Module 2 - Object Oriented Programming/7. The World of Objects/Folder/Main.java
+++ Module 2 - Object Oriented Programming/7. The World of Objects/Folder/Main.java
@@ -1,16 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2020;
-        nissan.color = "Green";
+        // Creating two object contstructors from my Car class blueprint
+        // Car nissan = new Car();
+        // nissan.make = "Nissan";
+        // nissan.price = 10000;
+        // nissan.year = 2020;
+        // nissan.color = "Green";
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 20000;
-        dodge.year = 2019;
-        dodge.color = "Blue";
+        // Car dodge = new Car();
+        // dodge.make = "Dodge";
+        // dodge.price = 20000;
+        // dodge.year = 2019;
+        // dodge.color = "Blue";
+
+    //   With use of cunstructor in the car class we create objects
+        Car nissan = new Car("Nissan", 10000, 2020, "Green");
+  
+        Car dodge = new Car("Dodge", 10000, 2019, "Blue");
+
+
+        
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price + 
         ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
